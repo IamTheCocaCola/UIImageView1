cfd644283e018c6c9f59e92f63c83766f46ba0d4
Sawyer Mallonee <temple77@comcast.net>
2023-01-10 10:11:49 -0700

    Initial Commit

---
+++ UIImageViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIImageViewDemo
+//
+//  Created by Mallonee, Sawyer - Student on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
